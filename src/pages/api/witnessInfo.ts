f2d6f9568062a47c73a5a2e4effe17196bba5d7d
DannyChain <thedannychain@gmail.com>
2021-02-17 19:23:13 -0300

    feat: add api endpoints
    That was the best way I found to go past the "mixed content" error when loading witness info.

---
+++ src/pages/api/witnessInfo.ts
@@ -0,0 +1,34 @@
+import { NowRequest, NowResponse } from "@vercel/node";
+
+export default async (request: NowRequest, response: NowResponse) => {
+  const { query } = request;
+
+  const { witness } = query;
+
+  if (!witness) {
+    response.status(400).send({
+      code: 400,
+      message: "The query param 'witness' is required on the URL.",
+    });
+  }
+
+  const data = await fetch(
+    `${process.env.WITNESS_URL_PROTOCOL || "http"}://${
+      process.env.WITNESS_NODE_IP || "35.188.146.63"
+    }:${process.env.WITNESS_NODE_PORT || "5000"}/contracts`,
+    {
+      headers: {
+        "content-type": "application/json",
+      },
+      body:
+        '{"jsonrpc":"2.0","method":"find","params":{"contract":"witnesses","table":"witnesses","query":{"account":"' +
+        witness +
+        '"}},"id": 9}',
+      method: "POST",
+    }
+  );
+
+  const json = (await data.json()).result;
+
+  response.json(json);
+};
