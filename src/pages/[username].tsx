9973156b567beaf324abdde65075482ea3bbec3d
DannyChain <thedannychain@gmail.com>
2021-02-17 22:59:05 -0300

    feat: prebuild pages for all known witnesses
    Closes #1

--- src/pages/[username].tsx
+++ src/pages/[username].tsx
@@ -3,28 +3,43 @@ import { useRouter } from "next/router";
 
 import Voter from "../components/voter";
 import Loading from "../components/loading";
-import { client } from "../services/dhive";
 
+import { getUserInfo } from "../services/hive";
+import {
+  getWitnessInfo,
+  getWitnessStatus,
+  getWitnessList,
+} from "../services/hiveEngine";
 
 import { IUserProps } from "../interfaces/general";
 
-const Witness: React.FC<IUserProps> = ({ username, userdata }) => {
+const Witness: React.FC<IUserProps> = ({
+  username,
+  userdata,
+  witnessInfo,
+  witnessStatus,
+}) => {
   const { isFallback } = useRouter();
 
   if (isFallback) {
     return <Loading />;
   }
 
-  return <Voter username={username} userdata={userdata} />;
+  return (
+    <Voter
+      username={username}
+      userdata={userdata}
+      witnessInfo={witnessInfo}
+      witnessStatus={witnessStatus}
+    />
+  );
 };
 
 export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = await getWitnessList();
+
   return {
-    paths: [
-      {
-        params: { username: "dannychain" },
-      },
-    ],
+    paths,
     fallback: true,
   };
 };
@@ -32,12 +47,40 @@ export const getStaticPaths: GetStaticPaths = async () => {
 export const getStaticProps: GetStaticProps = async (context) => {
   const { username } = context.params;
 
-  const userdata = await client.database.call("get_accounts", [[username]]);
+  const userdata = await getUserInfo(username);
+
+  const witnessInfo = await getWitnessInfo(username);
+
+  if (witnessInfo === undefined) {
+    return {
+      props: {
+        username,
+        userdata,
+        witnessInfo: "",
+        witnessStatus: "",
+      },
+    };
+  }
+
+  if (witnessInfo.enabled === false) {
+    return {
+      props: {
+        username,
+        userdata,
+        witnessInfo,
+        witnessStatus: "",
+      },
+    };
+  }
+
+  const witnessStatus = await getWitnessStatus(witnessInfo);
 
   return {
     props: {
       username,
-      userdata: userdata[0],
+      userdata,
+      witnessInfo,
+      witnessStatus,
     },
   };
 };
