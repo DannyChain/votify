b034df248b024afd18021d545a714ccc37ee9dac
DannyChain <thedannychain@gmail.com>
2021-02-17 22:59:05 -0300

    feat: prebuild pages for all known witnesses
    Closes #1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,65 @@
 import { GetStaticProps } from "next";
 
 import Voter from "../components/voter";
-import { client } from "../services/dhive";
 
+import { getWitnessInfo, getWitnessStatus } from "../services/hiveEngine";
 
 import { IUserProps } from "../interfaces/general";
+import { getUserInfo } from "../services/hive";
 
-const Home: React.FC<IUserProps> = ({ username, userdata }) => {
-  return <Voter username={username} userdata={userdata} />;
+const Home: React.FC<IUserProps> = ({
+  username,
+  userdata,
+  witnessInfo,
+  witnessStatus,
+}) => {
+  return (
+    <Voter
+      username={username}
+      userdata={userdata}
+      witnessInfo={witnessInfo}
+      witnessStatus={witnessStatus}
+    />
+  );
 };
 
 export const getStaticProps: GetStaticProps = async (context) => {
   const username = "dannychain";
 
-  const userdata = await client.database.call("get_accounts", [[username]]);
+  const userdata = await getUserInfo(username);
+
+  const witnessInfo = await getWitnessInfo(username);
+
+  if (witnessInfo === undefined) {
+    return {
+      props: {
+        username,
+        userdata,
+        witnessInfo: "",
+        witnessStatus: "",
+      },
+    };
+  }
+
+  if (witnessInfo.enabled === false) {
+    return {
+      props: {
+        username,
+        userdata,
+        witnessInfo,
+        witnessStatus: "",
+      },
+    };
+  }
+
+  const witnessStatus = await getWitnessStatus(witnessInfo);
 
   return {
     props: {
       username,
-      userdata: userdata[0],
+      userdata,
+      witnessInfo,
+      witnessStatus,
     },
   };
 };
--- src/services/hive.ts
+++ src/services/hive.ts
@@ -2,9 +2,25 @@ import { client } from "../services/dhive";
 
 export const getUserInfo = async (username) => {
   try {
-    const data = await client.database.call("get_accounts", [[username]]);
+    let userdata = {
+      name: "",
+      about: "",
+      witness_owner: "",
+    };
 
-    return JSON.stringify(data[0]);
+    const data = await client.database.call("get_accounts", [[username]]);
+    const postingJsonMetadata = data[0].posting_json_metadata;
+
+    if (postingJsonMetadata !== "") {
+      const { profile } = JSON.parse(postingJsonMetadata);
+      userdata = {
+        name: profile.name ?? "",
+        about: profile.about ?? "",
+        witness_owner: profile.witness_owner ?? "",
+      };
+    }
+
+    return userdata;
   } catch (error) {
     return error;
   }
