c884e2fc685e3d8adce68b2f1467d6216d61be52
DannyChain <thedannychain@gmail.com>
2021-04-22 14:06:33 -0300

    fix: remove info for now to get around build error

--- src/services/hive.ts
+++ src/services/hive.ts
@@ -8,17 +8,17 @@ export const getUserInfo = async (username) => {
       witness_owner: "",
     };
 
-    const data = await client.database.call("get_accounts", [[username]]);
-    const postingJsonMetadata = data[0].posting_json_metadata;
+    // const data = await client.database.call("get_accounts", [[username]]);
+    // const postingJsonMetadata = data[0].posting_json_metadata;
 
-    if (postingJsonMetadata !== "") {
-      const { profile } = JSON.parse(postingJsonMetadata);
-      userdata = {
-        name: profile.name ?? "",
-        about: profile.about ?? "",
-        witness_owner: profile.witness_owner ?? "",
-      };
-    }
+    // if (postingJsonMetadata !== "") {
+    //   const { profile } = JSON.parse(postingJsonMetadata);
+    //   userdata = {
+    //     name: profile.name ?? "",
+    //     about: profile.about ?? "",
+    //     witness_owner: profile.witness_owner ?? "",
+    //   };
+    // }
 
     return userdata;
   } catch (error) {
